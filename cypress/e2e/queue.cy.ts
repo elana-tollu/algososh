a34be36faf80b42771d931dbbdec03ebb032044a
Elana Tollu <elana.tollu@yandex.ru>
2022-10-16 17:50:09 +0300

    add e2e test for queue dequeue and clean

--- cypress/e2e/queue.cy.ts
+++ cypress/e2e/queue.cy.ts
@@ -91,5 +91,53 @@ describe('queue', () => {
             .should('have.data', 'cyState', 'default')
     })
 
+    it('dequeue', () => {
+        cy.get('[data-cy="item-to-add"]').type('A')
+        cy.contains('Добавить').click()
+        cy.get('[data-cy="item-to-add"]').type('B')
+        cy.contains('Добавить').click()
 
+        cy.contains('Удалить').click()
+        cy.get('[data-cy="circle"]').as('items0')
+            .should('have.length', 7)
+        cy.get('@items0').eq(0).as('items0-1')
+            .should('have.data', 'cyState', 'changing')
+        cy.get('[data-cy-enqueueing=false]').should('exist')   
+        
+        cy.get('@items0-1')
+            .find('[data-cy="main"]')
+            .should('have.text', 'A')
+        cy.get('@items0-1')
+            .find('[data-cy="head"]')
+            .should('have.text', 'head')
+        cy.get('@items0-1')
+            .find('[data-cy="tail"]')
+            .should('have.text', 'tail')
+        cy.get('@items0-1')
+            .find('[data-cy="index"]')
+            .should('have.text', '0')
+
+        cy.get('[data-cy-changing=false]').should('exist')   
+        cy.get('@items0-1')
+            .should('have.data', 'cyState', 'default')
+    })
+
+
+    it('clean', () => {
+        cy.get('[data-cy="item-to-add"]').type('A')
+        cy.contains('Добавить').click()
+        cy.get('[data-cy="item-to-add"]').type('B')
+        cy.contains('Добавить').click()
+        cy.get('[data-cy=circle][data-cy-state=default]').should('have.length', 7)
+
+        cy.contains('Очистить').click()
+        cy.get('[data-cy="circle"]').should('have.length', 7)
+            .each(circle => {
+                cy.wrap(circle)
+                    .should('have.data', 'cyState', 'default')
+                cy.wrap(circle)
+                    .find('[data-cy="main"]')
+                    .should('be.empty')
+            })   
+    })
 })
