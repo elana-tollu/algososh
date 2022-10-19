aaa91de91f013c8e95e0a944c75efcc1209f9e99
Elana Tollu <elana.tollu@yandex.ru>
2022-10-19 20:50:27 +0300

    extract expected Fibonacci sequence

--- cypress/e2e/fibonacci.cy.ts
+++ cypress/e2e/fibonacci.cy.ts
@@ -13,35 +13,36 @@ describe('fibonacci', () => {
     it('generates correctly', () => {
         cy.get('[data-cy="fibonacci-to-run"]').type('5')
         cy.contains('Рассчитать').click()
+        const expected = ['0', '1', '1', '2', '3']
 
         cy.get('[data-step="1"]')
         cy.get('[data-cy=circle]')
         .find('[data-cy=main]')
         .should('have.length', 1)
-        .last().should('have.text', '0')
+        .last().should('have.text', expected[0])
 
         cy.get('[data-step="2"]')
         cy.get('[data-cy=circle]')
         .find('[data-cy=main]')
         .should('have.length', 2)
-        .last().should('have.text', '1')
+        .last().should('have.text', expected[1])
 
         cy.get('[data-step="3"]')
         cy.get('[data-cy=circle]')
         .find('[data-cy=main]')
         .should('have.length', 3)
-        .last().should('have.text', '1')
+        .last().should('have.text', expected[2])
 
         cy.get('[data-step="4"]')
         cy.get('[data-cy=circle]')
         .find('[data-cy=main]')
         .should('have.length', 4)
-        .last().should('have.text', '2')
+        .last().should('have.text', expected[3])
 
         cy.get('[data-step="5"]')
         cy.get('[data-cy=circle]')
         .find('[data-cy=main]')
         .should('have.length', 5)
-        .last().should('have.text', '3')
+        .last().should('have.text', expected[4])
     })
 })
