0707950e899c67a7c77edb128cf19e81115af3b5
Elana Tollu <elana.tollu@yandex.ru>
2022-10-19 20:32:11 +0300

    fix test

--- cypress/e2e/fibonacci.cy.ts
+++ cypress/e2e/fibonacci.cy.ts
@@ -1,3 +1,5 @@
+/// <reference types="cypress" />
+
 describe('fibonacci', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/fibonacci')
@@ -13,27 +15,32 @@ describe('fibonacci', () => {
         cy.contains('Рассчитать').click()
 
         cy.get('[data-step="1"]')
-        cy.get('[data-cy="circle"]')
+        cy.get('[data-cy=circle]')
+        .find('[data-cy=main]')
         .should('have.length', 1)
         .last().should('have.text', '0')
 
         cy.get('[data-step="2"]')
-        cy.get('[data-cy="circle"]')
+        cy.get('[data-cy=circle]')
+        .find('[data-cy=main]')
         .should('have.length', 2)
         .last().should('have.text', '1')
 
         cy.get('[data-step="3"]')
-        cy.get('[data-cy="circle"]')
+        cy.get('[data-cy=circle]')
+        .find('[data-cy=main]')
         .should('have.length', 3)
         .last().should('have.text', '1')
 
         cy.get('[data-step="4"]')
-        cy.get('[data-cy="circle"]')
+        cy.get('[data-cy=circle]')
+        .find('[data-cy=main]')
         .should('have.length', 4)
         .last().should('have.text', '2')
 
         cy.get('[data-step="5"]')
-        cy.get('[data-cy="circle"]')
+        cy.get('[data-cy=circle]')
+        .find('[data-cy=main]')
         .should('have.length', 5)
         .last().should('have.text', '3')
     })
